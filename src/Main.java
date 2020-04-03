b19d9ef7536a8b91c641057f178ef8e8eb3e60cb
krishnasharma5547 <krishna.sharma_cs18@gla.ac.in>
2020-04-03 09:32:54 +0530

    Bubble sort program

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {20,40,10,50,30};
+        BubbleSort bs = new BubbleSort();
+        bs.bubbleSort(arr);
+        bs.printArray(arr);
+    }
+}
