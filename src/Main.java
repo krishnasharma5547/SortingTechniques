7f8dc41e2eef13812302030ce3f98377dd565644
krishnasharma5547 <krishna.sharma_cs18@gla.ac.in>
2020-04-03 15:31:19 +0530

    Insertion sort program

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static public int[] InputArray(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
     public static void main(String[] args) {
-        int arr[] = {20,40,10,50,30};
         BubbleSort bs = new BubbleSort();
+        int arr[] =  InputArray();
         bs.bubbleSort(arr);
         bs.printArray(arr);
+        int arr1[] = InputArray();
+        InsertionSort is = new InsertionSort();
+        is.sort(arr1);
+        is.printArray(arr1);
     }
 }
